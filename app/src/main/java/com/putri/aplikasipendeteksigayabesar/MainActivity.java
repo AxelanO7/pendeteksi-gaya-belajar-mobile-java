185fa11eea2373f9e1e5eea5e82b2e6b7baeb52a
AxelanO7 <axel@btwedutech.com>
2024-09-22 17:47:50 +0800

    refactor (project) : refactor code on main_activity

--- app/src/main/java/com/putri/aplikasipendeteksigayabesar/MainActivity.java
+++ app/src/main/java/com/putri/aplikasipendeteksigayabesar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.putri.aplikasipendeteksigayabesar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,17 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText username, password;
     Button btnLogin;
-    String userLogin [] = {
+    String[] userLogin = {
       "GungPutri", "MiaSwantari", "DanielSetiawan", "YulialdaMashun", "PatrisiaEtrianaBurga"
     };
-    String passLogin [] = {
+    String[] passLogin = {
       "pass1", "pass2", "pass3", "pass4", "pass5"
     };
-    static String namaUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,38 +32,39 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.editText2);
         btnLogin = findViewById(R.id.button);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String usernameKey = username.getText().toString();
-                String passwordKey = password.getText().toString();
-                if ((usernameKey.equals(userLogin[0]) && (passwordKey.equals(passLogin[0])))){
-                    succesLogin(usernameKey);
-                }else if((usernameKey.equals(userLogin[1]) && (passwordKey.equals(passLogin[1])))) {
-                    succesLogin(usernameKey);
-                }else if((usernameKey.equals(userLogin[2]) && (passwordKey.equals(passLogin[2])))){
-                    succesLogin(usernameKey);
-                }else if((usernameKey.equals(userLogin[3]) && (passwordKey.equals(passLogin[3])))){
-                    succesLogin(usernameKey);
-                }else if((usernameKey.equals(userLogin[4]) && (passwordKey.equals(passLogin[4])))){
-                    succesLogin(usernameKey);
-                }else if((usernameKey.equals("bk1")) && (passwordKey.equals("bk"))){
-                    Intent intent = new Intent(MainActivity.this, HelloAdmin.class);
-                    MainActivity.this.startActivity(intent);
-                    finish();
-                }else{
-                    failedLogin();
-                }
+        int bgColor = R.color.background;
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, bgColor));
+        getWindow().setNavigationBarColor(ContextCompat.getColor(MainActivity.this, bgColor));
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        btnLogin.setOnClickListener(v -> {
+            String usernameKey = username.getText().toString();
+            String passwordKey = password.getText().toString();
+            if ((usernameKey.equals(userLogin[0]) && (passwordKey.equals(passLogin[0])))){
+                succesLogin(usernameKey);
+            }else if((usernameKey.equals(userLogin[1]) && (passwordKey.equals(passLogin[1])))) {
+                succesLogin(usernameKey);
+            }else if((usernameKey.equals(userLogin[2]) && (passwordKey.equals(passLogin[2])))){
+                succesLogin(usernameKey);
+            }else if((usernameKey.equals(userLogin[3]) && (passwordKey.equals(passLogin[3])))){
+                succesLogin(usernameKey);
+            }else if((usernameKey.equals(userLogin[4]) && (passwordKey.equals(passLogin[4])))){
+                succesLogin(usernameKey);
+            }else if((usernameKey.equals("bk1")) && (passwordKey.equals("bk"))){
+                Intent intent = new Intent(MainActivity.this, HelloAdmin.class);
+                MainActivity.this.startActivity(intent);
+                finish();
+            }else{
+                failedLogin();
             }
         });
     }
 
     public void succesLogin(String usernameKey){
-        String nama = usernameKey;
         Toast.makeText(getApplicationContext(), "LOGIN SUKSES",
                 Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(MainActivity.this, StartPage.class);
-        intent.putExtra("username", nama);
+        intent.putExtra("username", usernameKey);
         MainActivity.this.startActivity(intent);
         finish();
     }
